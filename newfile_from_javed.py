acde12205cda61b2349adb2d23e9a072176eb702
Javed Iqbal <ji2481632@yahoo.com>
2026-08-07 21:54:43 -0400

    Added timestamp to the message

--- newfile_from_javed.py
+++ newfile_from_javed.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
+from datetime import datetime
 
 def main():
-    print("hello. My name is Inigo Montoya!")
+    ts = datetime.now().strftime("%Y-%M-%D %h:%m:%s")
+    print(f"Hello. My name is Inigo Montoya! {ts}")
 
 
 main()
